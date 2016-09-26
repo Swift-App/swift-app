a518b9df2687d242b151175ef87365958ce921cc
Rabin Gaire <rabingaire20@gmail.com>
2016-09-27 00:42:01 +0545

    Few Changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926160138) do
+ActiveRecord::Schema.define(version: 20160926182519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,19 @@ ActiveRecord::Schema.define(version: 20160926160138) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "name"
+    t.string   "first_name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "last_name"
+    t.string   "first_name_katakana"
+    t.string   "last_name_katakana"
+    t.integer  "phone"
+    t.datetime "birthday"
+    t.integer  "zip_1"
+    t.integer  "zip_2"
+    t.string   "prefecture"
+    t.string   "city"
+    t.string   "other_address"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
