cfb982214ea8cf35115b324a5ebf85438bc0327b
Rabin Gaire <rabingaire20@gmail.com>
2016-10-04 22:52:36 +0545

    Added action mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,15 +2,18 @@ class UserMailer < ApplicationMailer
   default from: 'notifications@swift.com'
 
   def welcome_email(user)
+    @user = user
     mail(to: @user.email, subject: 'Welcome to Swift')
   end
 
-  def reservation
-
+  def reservation(user)
+    @user = user
+    mail(to: @user.email, subject: 'Job reservation')
   end
 
-  def job_confirmation
-
+  def job_confirmation(user)
+    @user = user
+    mail(to: @user.email, subject: 'Job confirmation')
   end
 
   def report_completion(completion_report_params,user)
