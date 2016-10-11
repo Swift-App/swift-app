d00f158e4855e125c6d51dae2c131fc2d2299f31
Take Mouri <takehiromouri@gmail.com>
2016-10-11 16:47:58 +0545

    change columns

--- db/migrate/20161011101823_change_job_columns.rb
+++ db/migrate/20161011101823_change_job_columns.rb
@@ -2,6 +2,6 @@ class ChangeJobColumns < ActiveRecord::Migration[5.0]
   def change
     # remove_column :jobs, :pay_type
     # remove_column :jobs, :pay_amount
-    add_column :jobs, :pay_amount, :string
+    change_column :jobs, :pay_amount, :string
   end
 end
