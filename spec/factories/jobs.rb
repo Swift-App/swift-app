1ced3d086fc0463f4709afef204e15c840069c6f
Take Mouri <takehiromouri@gmail.com>
2016-10-22 17:40:35 +0545

    fix tests

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -2,15 +2,14 @@ FactoryGirl.define do
   factory :job do
     title " Job Title"
     description "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    pay_amount 1000
-    pay_type "pay type"
+    pay_amount "1000円"
     job_type "job type"
     content "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     location "location"
     nearest_station "nearest station"
-    number_of_positions 5
-    work_date Date.today + 5.days
-    hours 8
+    number_of_positions "5"
+    work_date "work date"
+    hours "8"
     holidays "no holidays"
     certifications_and_experience "none"
     benefits "none"
