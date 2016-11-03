001c5514a560693dccc300e7656576efed42dbe4
swojeet <swojeetkayastha@gmail.com>
2016-11-03 14:55:03 +0545

    save

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,3 @@
-$(function() {
-  $('#editor').morrigan_editor( {
-    iframeStyles: '/assets/iframe.css',
-    width:'770px',
-    height:'550px'
-  } );
-});
-
 $(".scroll-top").on("click", function(){
 	window.scrollTo(0,0);
 })
