c2df1785b7119ee784ec0b2506ab9b1421a4d39a
Take Mouri <takehiromouri@gmail.com>
2016-11-05 12:13:22 +0545

    japanese ckeditor

--- app/admin/news_article.rb
+++ app/admin/news_article.rb
@@ -16,7 +16,7 @@ permit_params :title, :content
 	form do |f|
 	  f.inputs do
 	    f.input :title
-	    f.input :content, :as => :ckeditor
+	    f.input :content, as: :ckeditor
 	  end
 	  f.actions
 	end
