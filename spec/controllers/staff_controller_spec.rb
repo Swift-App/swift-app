e507ee582a38b4b9204719fd2911dd8bdb1d4d85
Take Mouri <takehiromouri@gmail.com>
2016-11-13 10:01:49 +0545

    add spec

--- spec/controllers/staff_controller_spec.rb
+++ spec/controllers/staff_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe StaffController, type: :controller do
+  describe "GET #index" do
+    context "user is signed in" do
+      it "renders the index template" do
+        user = FactoryGirl.create(:user)
 
+        sign_in user
+        get :index
+
+        expect(response).to render_template :index
+      end     
+    end
+
+    context "user is not signed in" do
+      it "does not render the index template" do
+        get :index
+
+        expect(response).to redirect_to new_user_session_path
+      end     
+    end
+  end
 end
