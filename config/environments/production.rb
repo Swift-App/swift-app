2273b18188199062c6673370feae47462e058815
Take Mouri <takehiromouri@gmail.com>
2016-11-13 22:05:17 +0545

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,13 +74,15 @@ Rails.application.configure do
 
   
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'swift-haken.herokuapp.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'swift-haken.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
     :authentication => :plain,
+    :enable_starttls_auto => true
   }
+
   ActionMailer::Base.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
