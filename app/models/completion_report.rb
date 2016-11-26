8f1b10d178159ce9cce8276fe8c9d4f9d16e1eec
Takehiro <takehiromouri@gmail.com>
2016-11-26 15:57:40 +0545

    fix specs

--- app/models/completion_report.rb
+++ app/models/completion_report.rb
@@ -3,6 +3,6 @@ class CompletionReport < ApplicationRecord
 
   belongs_to :user  
   
-  validates :user_id, presence: true
+  validates :user_id, :start_time, :end_time, :job_name, presence: true
   validates :breaktime, length: {maximum: 3}
 end
--- spec/factories/completion_reports.rb
+++ spec/factories/completion_reports.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :completion_report do
     comments "It was a good day."
+    start_time Time.zone.now
+    end_time Time.zone.now + 30.minutes
   end
 end
