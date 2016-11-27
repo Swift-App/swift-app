a1e4c44551a40d372cf94733175b42e8f3bd54d1
Takehiro <takehiromouri@gmail.com>
2016-11-27 20:57:41 +0545

    change email for test purposes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
   
   default from: 'info@swift-staff.jp'
-  default to: "info@swift-staff.jp"
+  # default to: "info@swift-staff.jp"
+  default to: "takehiromouri@gmail.com"
 
   def attendance_report(args)
     @user = args.fetch(:user)
