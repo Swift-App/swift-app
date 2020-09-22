818ba3efcd3fa22ae6847e486361a579a232b6e8
tmouri <takehiromouri@gmail.com>
2020-09-22 10:00:03 -0700

    Fix email

--- app/models/company_contact.rb
+++ app/models/company_contact.rb
@@ -1,17 +1,17 @@
-class CompanyContact < MailForm::Base 
+class CompanyContact < MailForm::Base
   attr_accessor :terms_and_conditions
-  
+
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :company_name, :person_in_charge, validates: true
-  attribute :department, :furigana, :address_details, :website, :phone, 
+  attribute :department, :furigana, :address_details, :website, :phone,
             :postal_code, :prefecture, :city, :inquiry, :remarks
 
   validates_acceptance_of :terms_and_conditions
-  
+
   def headers
     {
       subject: "企業様お問い合わせ",
-      to: "info@info-swift.sakura.ne.jp",
+      to: "info@swift-staff.jp",
       from: %("#{company_name}" <#{email}>)
     }
   end
--- app/models/staff_contact.rb
+++ app/models/staff_contact.rb
@@ -1,16 +1,16 @@
-class StaffContact < MailForm::Base  
+class StaffContact < MailForm::Base
   attr_accessor :terms_and_conditions
-  
+
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :name, validate: true
   attribute :furigana, :birthday, :phone, :other
 
   validates_acceptance_of :terms_and_conditions
-  
+
   def headers
     {
       subject: "新規登録スタッフ",
-      to: "info@info-swift.sakura.ne.jp",
+      to: "info@swift-staff.jp",
       from: %("#{name}" <#{email}>)
     }
   end
